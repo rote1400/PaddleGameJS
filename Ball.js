44347690a40dac5f8c246ad37f10b2a21f32272e
rote1400 <rote1400@student.miun.se>
2024-02-17 00:16:00 +0100

    ball

--- Ball.js
+++ Ball.js
@@ -1,5 +1,45 @@
 export default class Ball {
     constructor(ballElem) {
         this.ballElem = ballElem;
+        this.reset();
     }
+
+    get x() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--x"));
+    }
+    
+    set x(value) {
+        this.ballElem.style.setProperty("--x", value);
+    }
+
+    get y() {
+        return parseFloat(getComputedStyle(this.ballElem).getPropertyValue("--y"));
+    }
+    
+    set y(value) {
+        this.ballElem.style.setProperty("--y", value);
+    }
+
+    reset() {
+        this.x = 50;
+        this.y = 50;
+        this.direction = { x: 0 };
+        while(
+            Math.abs(this.direction.x) <= 0.2 ||
+            Math.abs(this.direction.x) >= 0.9
+        ) {
+            const heading = randomNumberBetween(0, 2 * Math.PI);
+            this.direction = { x: Math.cos(heading), y: Math.sin(heading) };
+        }
+        console.log(this.direction);
+    }
+
+    update(delta) {
+        this.x = 5;
+        this.y = 15;
+    }
+}
+
+function randomNumberBetween(min, max) {
+    return Math.random() * (max - min) + min;
 }
