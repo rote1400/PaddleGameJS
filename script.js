4e339ee3d9bff15da031151784285de988b5ccc9
rote1400 <rote1400@student.miun.se>
2024-02-17 16:14:24 +0100

    paddle movement by mouse

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 import Ball from './Ball.js'
+import Paddle from './Paddle.js'
 
 const ball = new Ball(document.getElementById("ball"));
+const playerPaddle = new Paddle(document.getElementById("player-paddle"));
+const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
 
 // Update loop
 let lastTime;
@@ -11,7 +14,10 @@ function update(time) {
     }
     lastTime = time;
     window.requestAnimationFrame(update);
-    
 }
+
+document.addEventListener("mousemove", e => {
+    playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
 
 window.requestAnimationFrame(update);
