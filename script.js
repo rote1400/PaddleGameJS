4db553a40d3511e88f15097fe301e248ec6fe4c5
rote1400 <rote1400@student.miun.se>
2024-02-18 13:14:02 +0100

    game logic, collision

--- script.js
+++ script.js
@@ -4,21 +4,34 @@ import Paddle from './Paddle.js'
 const ball = new Ball(document.getElementById("ball"));
 const playerPaddle = new Paddle(document.getElementById("player-paddle"));
 const computerPaddle = new Paddle(document.getElementById("computer-paddle"));
+const playerScoreElement = document.getElementById("player-score");
+const computerScoreElement = document.getElementById("computer-score");
 
 // Update loop
 let lastTime;
 function update(time) {
     if (lastTime != null) {
         const delta = time - lastTime;
-        // ball.update(delta);
+        ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
         computerPaddle.update(delta, ball.y);
 
         if (isLose()) {
-            console.log("Lose");
+            handleLose();
         }
     }
     lastTime = time;
     window.requestAnimationFrame(update);
+}
+
+function handleLose() {
+    const rect = ball.rect();
+    if (rect.right >= window.innerWidth) {
+        playerScoreElement.textContent = parseInt(playerScoreElement.textContent) + 1;
+    } else {
+        computerScoreElement.textContent = parseInt(computerScoreElement.textContent) + 1;
+    }
+    ball.reset();
+    computerPaddle.reset();
 }
 
 function isLose() {
